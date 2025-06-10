089015f98fcdf229514c143d16bb8a0e75b28f6a
ahmdfaiizz <121543888+ahmdfaiizz@users.noreply.github.com>
2025-06-10 14:59:20 +0800

    Add project retrieval and debugging output in DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +10,18 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $projects = Project::withCount('scenarios')->get();
+
+        dd($projects); // Debugging line to check the projects
+
+        $projectCount = $projects->count(); // Count all projects
+        $activeCount = $projects->where('status', 'Active')->count();
+        $completedCount = $projects->where('status', 'Completed')->count();
+        $notstartedCount = $projects->where('status', 'Not Started')->count();
+
+        // dd($completedCount);
+        // dd($projectCount);
+
         return Inertia::render('Dashboard');
     }
 }
