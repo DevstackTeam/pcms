f2ffce93b8cbd55a1dfdee52e99dd0a07f58d7f6
balqisfakhira <balqissrozii@gmail.com>
2025-06-10 17:38:44 +0800

    Add MakeServiceCommand and service stub for service class generation

---
+++ app/Services/DashboardService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+class DashboardService
+{
+    /**
+     * Get Project statistics.
+     *
+     *@return array
+     */
+
+}
+
+
+
+
---
+++ app/Services/MyService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services;
+
+class MyService
+{
+    /**
+     * Get Project statistics.
+     *
+     *@return array
+     */
+}
