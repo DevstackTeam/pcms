ccd511d22dfaa7f8538d10830772be7c6c0f0aba
Firdaus Maliki <97101144+firdausmaliki@users.noreply.github.com>
2025-06-11 16:40:14 +0800

    Revert "Balqis"

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,36 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Inertia\Inertia;
-use App\Models\Project;
 use Illuminate\Http\Request;
-use App\Services\DashboardService;
-use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    protected $dashboardService;
-
-    public function __construct(
-        DashboardService $dashboardService
-    )
-    {
-        $this->dashboardService = $dashboardService;
-
-    }
-
     public function index()
     {
-        $data = $this->dashboardService->getProjectStatistics();
-        // dd($data['projects']);
-        return Inertia::render('Dashboard', [
-            'projects' => $data['projects'],
-            'projectCount' => $data['projectCount'],
-            'activeCount' => $data['activeCount'],
-            'completedCount' => $data['completedCount'],
-            'notstartedCount' => $data['notstartedCount'],
-            'latestProjects' => $data['latestProjects'],
-            'user' => Auth::user(),
-        ]);
+        return Inertia::render('Dashboard');
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,11 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp, Head } from '@inertiajs/vue3'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-import '../css/custom-bootstrap.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
-    title: (title) => `PCMS ${title}`,
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`]
-    },
+    resolve: name => require(`./Pages/${name}.vue`),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
-            .component('Head', Head)
             .mount(el)
     },
 })
