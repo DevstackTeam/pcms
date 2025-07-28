8fce90094221b005402ac24c31916c99247fbd0e
ahmdfaiizz <121543888+ahmdfaiizz@users.noreply.github.com>
2025-07-28 15:24:47 +0800

    feat: create pages for users including create, edit, and show

---
+++ resources/js/Pages/Users/Show.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container-fluid px-3 px-sm-4">
+    <Header iconClass="bi-person-badge" title="Users" />
+
+    <CardBox title="View User">
+      <div class="row">
+        <div class="col-md-6 mb-3">
+          <FormDetail label="Name">
+            {{ user.name }}
+          </FormDetail>
+        </div>
+
+        <div class="col-md-6 mb-3">
+          <FormDetail label="Email">
+            {{ user.email }}
+          </FormDetail>
+        </div>
+
+        <div class="col-md-6 mb-3">
+          <FormDetail label="Username">
+            {{ user.username }}
+          </FormDetail>
+        </div>
+      </div>
+
+      <div class="d-flex justify-content-end">
+        <Link href="/users" class="btn btn-outline-secondary">Close</Link>
+        <Link :href="`/users/${user.id}/edit`" class="btn btn-primary ms-2">Edit</Link>
+      </div>
+    </CardBox>
+  </div>
+</template>
+
+<script setup>
+import SidebarLayout from '@/Layouts/SidebarLayout.vue';
+import Header from '@/Components/Header.vue'
+import CardBox from '@/Components/CardBox.vue'
+import FormDetail from '@/Components/FormDetail.vue'
+import { Link } from '@inertiajs/vue3';
+
+defineOptions({
+  layout: SidebarLayout
+})
+
+const props = defineProps({
+  user: Object
+})
+</script>
