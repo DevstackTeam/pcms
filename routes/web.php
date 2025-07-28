a1c91a31269bf3f86c55b68a8b1437684d9b82f1
ahmdfaiizz <121543888+ahmdfaiizz@users.noreply.github.com>
2025-07-28 10:30:07 +0800

    feat: create route for roles and users

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\ScenarioController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DesignationController;
+use App\Http\Controllers\RoleController;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect()->route('login');
@@ -23,6 +25,11 @@ Route::middleware('auth')->group(function () {
     Route::resource('designations', DesignationController::class)->except('create', 'edit', 'show');
     Route::resource('projects', ProjectController::class);
     Route::resource('projects.scenarios', ScenarioController::class);
+    Route::resource('roles', RoleController::class);
+
+    Route::get('/users', function () {
+        return Inertia::render('Users');
+    });
 });
 
 Route::middleware('auth:sanctum')->group(function () {
