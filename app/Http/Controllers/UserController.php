30cdeee8a783e6087e1b2f6f8dd763fafc717453
ahmdfaiizz <121543888+ahmdfaiizz@users.noreply.github.com>
2025-07-28 15:23:03 +0800

    feat: define method for resource controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Inertia\Inertia;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
@@ -11,7 +15,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Users/Index', [
+            'users' => User::with('roles')->get() 
+        ]);
     }
 
     /**
@@ -19,7 +25,9 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Users/Create', [
+            'roles' => Role::pluck('name')->all()
+        ]);
     }
 
     /**
@@ -27,38 +35,76 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::create(
+            $request->only(['name', 'email', 'username']) +
+            ['password' => Hash::make($request->password)]
+        );
+
+        $user->syncRoles($request->roles);
+
+        return redirect()->route('users.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $user)
     {
-        //
+        return Inertia::render('Users/Show', [
+            'user' => $user
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(User $user)
     {
-        //
+        return Inertia::render('Users/Edit', [
+            'user' => $user,
+            'userRoles' => $user->roles()->pluck('name')->all(),
+            'roles' => Role::pluck('name')->all(),
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'username' => 'required',
+        ]);
+
+        $data = $request->only(['name', 'email', 'username']);
+
+        if ($request->filled('password')) {
+            $data['password'] = Hash::make($request->password);
+        }
+
+        $user->update($data);
+
+        $user->syncRoles($request->roles);
+
+        return redirect()->route('users.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect()->route('users.index');
     }
 }
